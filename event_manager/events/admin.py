cb6aefb770e1472f992091cc33281909a7c7632e
EgloZ938 <theo.evon.2004@gmail.com>
2025-04-30 14:35:27 +0200

    add setting.py et contenu de admin.py

--- event_manager/events/admin.py
+++ event_manager/events/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Event, Participation
 
-# Register your models here.
+class ParticipationInline(admin.TabularInline):
+    model = Participation
+    extra = 0
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('title', 'date', 'created_by', 'get_participant_count')
+    list_filter = ('date', 'created_by')
+    search_fields = ('title', 'description')
+    date_hierarchy = 'date'
+    inlines = [ParticipationInline]
+
+@admin.register(Participation)
+class ParticipationAdmin(admin.ModelAdmin):
+    list_display = ('user', 'event', 'created_at')
+    list_filter = ('event', 'created_at')
+    search_fields = ('user__username', 'event__title')
